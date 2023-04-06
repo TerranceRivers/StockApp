abcdb89b6ee239148e48037b75e0bc5315476ca4
TerranceRivers <109315955+TerranceRivers@users.noreply.github.com>
2023-04-06 19:55:53 -0400

    version1.1

--- trading.js
+++ trading.js
@@ -1,54 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
-  loadTrades();
-
-  const tradesForm = document.getElementById("tradesForm");
-  tradesForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    addTrade();
+$(document).ready(function() {
+    // Initialize trades array
+    var trades = [];
+  
+    // Handle form submission
+    $('#tradesForm').submit(function(event) {
+      event.preventDefault();
+      var symbol = $('#tradeSymbol').val();
+      var price = $('#tradePrice').val();
+      var quantity = $('#tradeQuantity').val();
+      var notes = $('#tradeNotes').val();
+  
+      // Add trade to array
+      var trade = {
+        symbol: symbol,
+        price: price,
+        quantity: quantity,
+        notes: notes
+      };
+      trades.push(trade);
+  
+      // Update trades table
+      var tableBody = $('#tradesTable');
+      var row = $('<tr>');
+      row.append($('<td>').text(trade.symbol));
+      row.append($('<td>').text(trade.price));
+      row.append($('<td>').text(trade.quantity));
+      var totalCost = trade.price * trade.quantity;
+      row.append($('<td>').text(totalCost.toFixed(2)));
+      row.append($('<td>').text(trade.notes));
+      tableBody.append(row);
+  
+      // Clear form
+      $('#tradesForm')[0].reset();
+    });
   });
-});
-
-function addTrade() {
-  const tradeSymbol = document.getElementById("tradeSymbol").value;
-  const tradePrice = parseFloat(document.getElementById("tradePrice").value);
-  const tradeQuantity = parseInt(document.getElementById("tradeQuantity").value, 10);
-  const tradeNotes = document.getElementById("tradeNotes").value;
-
-  const trade = {
-    symbol: tradeSymbol,
-    price: tradePrice,
-    quantity: tradeQuantity,
-    totalCost: tradePrice * tradeQuantity,
-    notes: tradeNotes,
-  };
-
-  let trades = getTradesFromLocalStorage();
-  trades.push(trade);
-  localStorage.setItem("trades", JSON.stringify(trades));
-  loadTrades();
-  document.getElementById("tradesForm").reset();
-}
-
-function loadTrades() {
-  const trades = getTradesFromLocalStorage();
-  let tradesHtml = "";
-
-  for (const trade of trades) {
-    tradesHtml += `
-      <tr>
-        <td>${trade.symbol}</td>
-        <td>${trade.price.toFixed(2)}</td>
-        <td>${trade.quantity}</td>
-        <td>${trade.totalCost.toFixed(2)}</td>
-        <td>${trade.notes}</td>
-      </tr>
-    `;
-  }
-
-  document.getElementById("tradesTable").innerHTML = tradesHtml;
-}
-
-function getTradesFromLocalStorage() {
-  const trades = localStorage.getItem("trades");
-  return trades ? JSON.parse(trades) : [];
-}
+  
